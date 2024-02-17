44b626a625c78ed9366b4cb382261e0cf5d3dd7b
Ricardo Maloloy-on <ricky_maloy@yahoo.com>
2024-02-17 11:52:51 +0800

    update-gh-actions
    Signed-off-by: Ricardo Maloloy-on <ricky_maloy@yahoo.com>

---
+++ update-badge.js
@@ -0,0 +1,17 @@
+// Example: Fetch number of followers from GitHub API
+const fetch = require('node-fetch');
+
+async function getFollowers(username) {
+  const response = await fetch(`https://api.github.com/users/${username}`);
+  const data = await response.json();
+  return data.followers;
+}
+
+async function updateBadge() {
+  const username = 'rmaloloyon'; // Replace with your GitHub username
+  const followers = await getFollowers(username);
+
+  console.log(`::set-output name=followers::${followers}`);
+}
+
+updateBadge();
